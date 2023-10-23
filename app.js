ae60887102ef2f0f1c2524161063f0c55aeefae8
muhammad45401 <saadjani3333@gmail.com>
2023-10-23 22:20:55 +0500

    password

---
+++ app.js
@@ -0,0 +1,22 @@
+var password =prompt("enter you password")
+
+
+ if(password == 123456){
+    alert("“Correct! The password you entered matches the original password”.")
+
+
+
+ }else {
+    alert("“Incorrect password”");} 
+ 
+
+
+
+
+
+
+
+
+
+
+
